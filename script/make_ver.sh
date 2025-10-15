af7cc17187a51448027d14a70e30694d8ea6b1b5
shu-htk <shu-htk@outlook.jp>
2025-10-15 17:34:19 +0900

    script/make_ver.sh: if git is exist

--- script/make_ver.sh
+++ script/make_ver.sh
@@ -1,18 +1,21 @@
 #!/bin/bash
 
 if [ -e .git ] ; then
-    git_tag=`git describe --tags --always`
-    git_log=`git log -1 --pretty=format:'%ad, %an' --date=iso`
-    echo "$git_tag $git_log" > ver.tmp
-    if [ -e version.txt ] ; then
-	update=`cmp ver.tmp version.txt | grep -c .`
-	if [ $update == 1 ] ; then
-	    echo "upadate version.txt"
+    if command -v git > /dev/null ; then
+	git_tag=`git describe --tags --always`
+	git_log=`git log -1 --pretty=format:'%ad, %an' --date=iso`
+    
+	echo "$git_tag $git_log" > ver.tmp
+	if [ -e version.txt ] ; then
+	    update=`cmp ver.tmp version.txt | grep -c .`
+	    if [ $update == 1 ] ; then
+		echo "upadate version.txt"
+		cp ver.tmp version.txt
+	    fi
+	else
+	    echo "create version.txt"
 	    cp ver.tmp version.txt
 	fi
-    else
-	echo "create version.txt"
-	cp ver.tmp version.txt
+	rm -f ver.tmp
     fi
-    rm -f ver.tmp
 fi
