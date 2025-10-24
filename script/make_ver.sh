6c652edfc5ea8892bc31e8b993a8a6a01951e508
shu-htk <shu-htk@outlook.jp>
2025-10-24 17:14:58 +0900

    modify script/make_ver.sh

--- script/make_ver.sh
+++ script/make_ver.sh
@@ -4,13 +4,15 @@ if [ -e .git ] ; then
     if command -v git > /dev/null ; then
 	git_tag=`git describe --tags --always`
 	git_log=`git log -1 --pretty=format:'%ad, %an' --date=iso`
-    
 	echo "$git_tag $git_log" > ver.tmp
+	echo "$git_tag $git_log"
 	if [ -e version.txt ] ; then
 	    update=`cmp ver.tmp version.txt | grep -c .`
 	    if [ $update == 1 ] ; then
-		echo "upadate version.txt"
+		echo "version.txt is updated"
 		cp ver.tmp version.txt
+	    else
+		echo "version.txt is not updated"
 	    fi
 	else
 	    echo "create version.txt"
